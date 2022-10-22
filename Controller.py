711db824a6c0327f22eb83f89bfb5e48801c4f17
Alex <alex.kojokari42@gmail.com>
2022-10-22 14:10:11 +0300

    Modified View and Controller and added Main

--- Controller.py
+++ Controller.py
@@ -1,7 +1,10 @@
 import Model
 import View
 
+
 def button_click():
     value_a = View.get_value()
     value_b = View.get_value()
-    Model.init()
+    Model.init(value_a, value_b)
+    result = Model.sum()
+    View.view_data(result)
--- View.py
+++ View.py
@@ -1,2 +1,6 @@
-def view(data):
-    print(data)
+def view_data(data):
+    print(f'The result is {data}')
+
+
+def get_value():
+    return int(input('Value = '))
