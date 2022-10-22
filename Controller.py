9dc0288e7a4f29819e5d40da0f74b7e4e5fea647
Alex <alex.kojokari42@gmail.com>
2022-10-22 14:12:45 +0300

    Changed name for Model

--- Controller.py
+++ Controller.py
@@ -1,10 +1,10 @@
-import Model
+import Model_Sum
 import View
 
 
 def button_click():
     value_a = View.get_value()
     value_b = View.get_value()
-    Model.init(value_a, value_b)
-    result = Model.sum()
+    Model_Sum.init(value_a, value_b)
+    result = Model_Sum.sum()
     View.view_data(result)
